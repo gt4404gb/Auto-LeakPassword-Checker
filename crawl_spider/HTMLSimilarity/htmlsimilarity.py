d81914bb71242ae9301fcbbd8266955ac4e3f1ce
gt4404gb <1178878380@qq.com>
2022-11-25 15:21:56 +0800

    新增csv导入文件支持，新增网页相似度判断，减少重复爆破数量

---
+++ crawl_spider/HTMLSimilarity/htmlsimilarity.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: taoqi
+@file: HTMLSimilarity.py
+@time: 2019-07-02 16:57
+"""
+
+from crawl_spider.HTMLSimilarity.htmlparser import HTMLParser
+from crawl_spider.HTMLSimilarity.domtree2data import Converter
+from crawl_spider.HTMLSimilarity.calc import calculated_similarity
+
+
+def get_html_similarity(html_doc1, html_doc2, dimension=5000):
+    hp1 = HTMLParser(html_doc1)
+    html_doc1_dom_tree = hp1.get_dom_structure_tree()
+    hp2 = HTMLParser(html_doc2)
+    html_doc2_dom_tree = hp2.get_dom_structure_tree()
+    converter = Converter(html_doc1_dom_tree, dimension)
+    dom1_eigenvector = converter.get_eigenvector()
+    converter = Converter(html_doc2_dom_tree, dimension)
+    dom2_eigenvector = converter.get_eigenvector()
+    value = calculated_similarity(dom1_eigenvector, dom2_eigenvector, dimension)
+    if value > 0.02:
+        return False
+    else:
+        return True
